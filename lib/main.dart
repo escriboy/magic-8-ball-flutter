4a2c96594c98a99b79be081632940b03fbc938fe
YM CHOI <escriboy@gmail.com>
2023-06-19 00:19:00 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
+import 'dart:math'; // dart:math 라이브러리 추가
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0; // ballNumber 변수 생성 및 초기화
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+            print(ballNumber);
+          });
+        },
+        child:
+            Image.asset('images/ball$ballNumber.png'), // ballNumber에 따라 이미지 변경
+      ),
+    );
+  }
+}
